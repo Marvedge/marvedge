c2b25daf4c18d6f2d4c313c7ae775456890dee6c
Brijesh Shukla <bkshukla229@gmail.com>
2025-11-03 10:08:13 +0530

    fix:updates props(TimelineRuler) in EditorClient.backup.tsx

--- app/(signed)/editor/EditorClient.backup.tsx
+++ app/(signed)/editor/EditorClient.backup.tsx
@@ -1619,6 +1619,8 @@ export default function EditorPage() {
                 onZoomEffectCreate={onZoomEffectCreate}
                 initialSegments={currentSegments}
                 onTrim={videoTrimHandler}
+                playing={playing}
+                setPlaying={setPlaying}
               />
             ) : (
               <div className="w-full max-w-6xl mx-auto">
